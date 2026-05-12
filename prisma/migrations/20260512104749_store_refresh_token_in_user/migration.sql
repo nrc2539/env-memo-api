360d28c35d325b3b3e3d606e0710e47a2d0d00a5
nrc-dev <narongchai.b2539@gmail.com>
2026-05-12 18:30:36 +0700

    fix: remove role from user

--- prisma/migrations/20260512104749_store_refresh_token_in_user/migration.sql
+++ prisma/migrations/20260512104749_store_refresh_token_in_user/migration.sql
@@ -1,15 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `RefreshToken` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "RefreshToken" DROP CONSTRAINT "RefreshToken_userId_fkey";
-
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "refreshToken" TEXT,
 ADD COLUMN     "refreshTokenExpiry" TIMESTAMP(3);
-
--- DropTable
-DROP TABLE "RefreshToken";
--- prisma/migrations/20260512111842_add_role_to_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('OWNER', 'EDITOR', 'VIEWER');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'VIEWER';
