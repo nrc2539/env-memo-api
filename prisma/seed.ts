78ef4105f27d940da63591c0fcdd36700b4af329
nrc-dev <narongchai.b2539@gmail.com>
2026-05-13 13:10:41 +0700

    feat: add seed data

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../src/generated/client';
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+
+async function main() {
+  console.log('Start seeding...');
+  const hashedPassword =
+    '$2b$10$8CtHuB0kWzm7TY6egL.RrucCJs79zq9oEqw15LuqIHOxhHObtbXNq';
+
+  const user = await prisma.user.upsert({
+    where: { email: 'admin@email.com' },
+    update: {},
+    create: {
+      email: 'admin@email.com',
+      password: hashedPassword,
+    },
+  });
+
+  console.log('Seeded user:', user.email);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
