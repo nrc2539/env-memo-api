8e298ae2506cc4691582ac03916c5a040f74cef6
nrc-dev <narongchai.b2539@gmail.com>
2026-05-18 21:31:08 +0700

    fix: update env API_PORT

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
 
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(process.env.API_PORT ?? 8080);
 }
 void bootstrap();
